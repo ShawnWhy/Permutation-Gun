ef677ecebf7a22506f021c1bf274b005742b9c11
Shawn Yu <shawnyudesign@gmail.com>
2023-03-14 00:42:47 -0400

    the tank is in

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 import style from './main.css'
 import axios from "axios";
+import $ from "jquery";
+var spokeInterval;
 
 function swap(a, b, array) {
     newArray = array
@@ -82,7 +84,7 @@ const mainFunction2 = async function(string){
 
 var permutationArray = [];
 var originalStep= 0;
-
+console.log(string)
 var StringToArray = string.split('');
 
 recursionFunction2(originalStep, StringToArray, permutationArray);
@@ -139,33 +141,165 @@ const recursionFunction2 = function(step, array, permutationArray){
     }
 }
 
+var mockResponse = ['cat', 'cst', 'abc', 'caty', 'act', 'tba', 'bat', 'tab', 'ats', 'bats', 'sta', 'bas', 'sba', 'abs', 'cyst', 'bay', 'say', 'acts', 'sty', 'cyb', 'cbs', 'stacy', 'stab', 'tabs', 'cay', 'tbs', 'cab', 'scat', 'sac', 'bsa', 'cabs', 'bays', 'sat', 'cats', 'cast', 'stay', 'bast', 'cays', 'scab'];
 
 
-mainFunction2("catsby").then((resultArray)=>{
-console.log(resultArray)
+  // Using util.promisify would look nicer, but there is a lolex issue
+  // blocking this at the moment: https://github.com/sinonjs/lolex/pull/227
 
-axios.post("/api/getSpellCheck",resultArray).then(res=>{
-    if(res.data!==null){
-        console.log(res.data);
-    }
-}) .catch((err) => {
-console.log(err);
-})
-// resultArray.forEach(element => {
-// setTimeout(() => {
-    
 
-// console.log(element);
-// if(element !== null){
-// axios.get("/api/getSpellCheck",{word:element}).then(res=>{
+const getWords = async function(input){
+console.log("getting words");
+console.log(input);
+  const setTimeoutPromise = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+  await setTimeoutPromise(5000);
+  return mockResponse;
+
+
+
+mainFunction2(input).then((resultArray)=>{
+    resultArray.forEach(result=>{
+        var variant = $("<div>")
+        $(variant).html(result)
+        $("body").push(variant);
+    })
+// console.log(resultArray)
+//temporarily disabled to test the front end
+// axios.post("/api/getSpellCheck",resultArray).then(res=>{
+//     if(res.data!==null){
 //         console.log(res.data);
-    
+//     }
 // }) .catch((err) => {
 // console.log(err);
-
 // })
-// }
-// }, 1800);    
+})
+  
+};
+
+
+
+const constructGun= ()=>{
+console.log("constructing gun")
+setTimeout(() => {
+    $(".barrel").addClass("moveBarrel")
+    
+}, 5000);
+$(".gunContainer").addClass("moveGun")
+var wheel = $("<div>")
+$(wheel).addClass("wheel");
+for(let i=0; i<20; i++){
+    let spoke = $("<div>")
+    $(spoke).addClass("spoke")
+    $(spoke).css("transform","rotate("+ 13*i+"deg)")
+    wheel.append(spoke);
+    if(i==19){
+        $('body').append(wheel);
+    spokeSetColors() 
+     spokeInterval = setInterval(() => {
+     spokeSetColors() 
+     },1000);
+
+
+    }
+  }
+}
+
+ var newTitleColors = [
+    'blue','rgb(244, 170, 42)',
+    'rgb(255, 10, 247)','rgb(204, 255, 51)','rgb(242, 115, 208)',
+    'rgb(255, 51, 0)',"green"
+  ]
+
+//use the async gotten words from the array to use as an array to create
+//bullets for the gun 
+
+
+function spokeSetColors(){
+var spokes = $(".wheel .spoke");
+console.log(spokes)
+for(let i = 0; i<spokes.length; i++){
+    setTimeout(() => {
+    var randNumber = Math.floor(Math.random()*newTitleColors.length)
+    var randColor = newTitleColors[randNumber];
+    $(spokes[i]).css("background-color",randColor);
+        
+    }, 100*i);
+}
+}
+
+
+
+
+
+
+
+
+//jquer input start
+var inputForm = $(".inputForm");
+$(inputForm).on("submit",(e)=>{
+    e.stopPropagation();
+    e.preventDefault();
+    //use the letter to call the API to get the list of words back
+    submitNewLetter();
+
+    
+
+}
+)
+function createBullets(response){
+    console.log("createBullets")
+    // make the wheel dissapear
+    $(".wheel").remove()
+    clearInterval(spokeInterval)
+    for(let i=0; i<response.length; i++){
+    setTimeout(() => {
+     var bullet = $("<div>")
+    var randNumber = Math.floor(Math.random()*newTitleColors.length)
+    var randColor = newTitleColors[randNumber];
+    $(bullet).css("background-color",randColor);
+    $(bullet).css("height",Math.floor(300/response.length)+"px")
+    $(bullet).attr("letter",response[i])
+    $(".magazine").append(bullet);
+
+     }, i*100);
+
+    }
+
+}
+
+function submitNewLetter(){
+var input = $(".typeInput")
+
+var value= $(input).val();
+console.log(value);
+if(value.length<=5){
+constructGun();
+getWords(value).then(response=>{
+    console.log("got response");
+    console.log(response);
+createBullets(response)
+
 });
-// }
-// )
+
+mainFunction2(value).then((resultArray)=>{
+    console.log(resultArray)
+    resultArray.forEach(result=>{
+        var variant = $("<div>")
+        $(variant).html(result)
+        $(variant).addClass("variant")
+        $("body").append(variant);
+    })
+// console.log(resultArray)
+//temporarily disabled to test the front end
+// axios.post("/api/getSpellCheck",resultArray).then(res=>{
+//     if(res.data!==null){
+//         console.log(res.data);
+//     }
+// }) .catch((err) => {
+// console.log(err);
+// })
+})
+}
+}
